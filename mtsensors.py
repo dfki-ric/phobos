2b2f385cac7dc1e63dcf50a2eafdb4bdb0428c0e
Kai von Szadkowski <kavo01@dfki.de>
2014-01-16 17:21:30 +0100

    blendertools: implemented automatic adding of selected nodes and joints to new sensors

--- mtsensors.py
+++ mtsensors.py
@@ -23,7 +23,7 @@ def unregister():
 
 
 class AddSensorOperator(Operator):
-    """ExportModelOperator"""
+    """AddSensorOperator"""
     bl_idname = "object.mt_add_sensor"
     bl_label = "Add a node-dependent sensor"
     bl_options = {'REGISTER', 'UNDO'}
@@ -38,17 +38,32 @@ class AddSensorOperator(Operator):
     def execute(self, context):
         location = bpy.context.scene.cursor_location
         if self.sensor_type in mtdefs.sensorTypes:
-            mtutility.createPrimitive(self.sensor_type, self.sensor_type, 10, location, mtdefs.layerTypes["sensors"], "sensor")
-            sensor = bpy.context.scene.objects.active
-            if "Node" in self.sensor_type:
-                #TODO; add sensor nodes
-                print("Added nodes to new " + self.sensor_type)
-            elif "Joint" in self.sensor_type:
-                #TODO; add sensor joints
-                print("Added nodes to new " + self.sensor_type)
-            elif "Motor" in self.sensor_type:
-                #TODO; add sensor joints
-                print("Added nodes to new " + self.sensor_type)
+            if "Node" in self.sensor_type or "Joint" in self.sensor_type or "Motor" in self.sensor_type:
+                i = 1
+                objects = []
+                for obj in bpy.context.selected_objects:
+                    objects.append(obj)
+                mtutility.createPrimitive(self.sensor_type, self.sensor_type, 0.1, location, mtdefs.layerTypes["sensors"], "sensor")
+                sensor = bpy.context.scene.objects.active
+                sensor.MARStype = "sensor"
+                if "Node" in self.sensor_type:
+                    for obj in objects:
+                        if obj.MARStype == "body":
+                            sensor["index"+str(i)] = obj.name
+                            i += 1
+                    print("Added nodes to new " + self.sensor_type)
+                elif "Joint" in self.sensor_type:
+                    for obj in objects:
+                        if obj.MARStype == "joint":
+                            sensor["index"+str(i)] = obj.name
+                            i += 1
+                    print("Added nodes to new " + self.sensor_type)
+                elif "Motor" in self.sensor_type:
+                    for obj in objects:
+                        if obj.MARStype == "joint":
+                            sensor["index"+str(i)] = obj.name
+                            i += 1
+                    print("Added nodes to new " + self.sensor_type)
             else: #visual sensor
                 if self.sensor_type == "RaySensor":
                     print("Added nodes to new " + self.sensor_type)
@@ -59,28 +74,4 @@ class AddSensorOperator(Operator):
         else:
             print("Sensor could not be created: unknown sensor type.")
         return {'FINISHED'}
-
-
-    sensorTypes = ["RaySensor",
-               "CameraSensor",
-               "ScanningSonar",
-
-               "JointPosition",
-               "JointVelocity",
-               "JointLoad",
-               "JointTorque",
-               "JointAVGTorque",
-               "Joint6DOF",
-
-               "NodeContact",
-               "NodePosition",
-               "NodeRotation",
-               "NodeContactForce",
-               "NodeCOM",
-               "NodeVelocity",
-               "NodeAngularVelocity",
-
-               "MotorCurrent"
-              ]
-
 
