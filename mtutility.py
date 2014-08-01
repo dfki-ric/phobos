b8597aec5375b5f29e95121fa520fd74605e91fd
Kai von Szadkowski <kavo01@dfki.de>
2014-08-01 14:57:05 +0200

    blendertools: bugfixes, documentation and reformatting for mtutility

--- mtutility.py
+++ mtutility.py
@@ -19,6 +19,7 @@ import marstools.mtmaterials as mtmaterials
 import marstools.mtdefs as mtdefs
 from datetime import datetime
 
+
 def register():
     print("Registering mtutility...")
 
@@ -26,7 +27,9 @@ def register():
 def unregister():
     print("Unregistering mtutility...")
 
+
 def is_float(s):
+    '''Tests if an input variable (string) is a float number.'''
     try:
         float(s)
         return True
@@ -34,13 +37,17 @@ def is_float(s):
         return False
 
 def is_int(s):
+    '''Tests is an input variable (string) is an int number.'''
     try:
         int(s)
         return True
     except ValueError:
         return False
 
+
 def parse_number(s):
+    '''Takes an input variable (string) and determines whether it represents
+    a float number, int or string'''
     if is_int(s):
         return int(s)
     elif is_float(s):
@@ -48,40 +55,58 @@ def parse_number(s):
     else:
         return s
 
+
 def only_contains_int(stringlist):
+    '''Checks if a list of strings contains int numbers exclusively.'''
     for num in stringlist:
         if not is_int(num):
             return False
     return True
 
+
 def only_contains_float(stringlist):
+    '''Checks if a list of strings contains float numbers exclusively.'''
     for num in stringlist:
         if not is_float(num):
             return False
     return True
 
-def find_in_list(alist, prop, value):
-    n = -1
 
+def find_in_list(alist, prop, value):
+    '''Returns the index of the first object in a list which has a field
+    named *prop* with value *value*. If no such object is found, returns -1.'''
+    n = -1
     for i in range(len(alist)):
-        if alist[i][prop] == value:
-            n = i
-            break
+        try:
+            if alist[i][prop] == value:
+                n = i
+                break
+        except KeyError:
+            pass
     return n
 
-def retrieve_from_list(alist, prop, value):
-    n = -1
 
+def retrieve_from_list(alist, prop, value):
+    '''Returns the first object in a list which has a field named
+    *prop* with value *value*. If no such object is found, returns "None".'''
+    n = -1
     for i in range(len(alist)):
-        if alist[i][prop] == value:
-            n = i
-            break
+        try:
+            if alist[i][prop] == value:
+                n = i
+                break
+        except KeyError:
+            pass
     if n >= 0:
         return alist[n][prop]
     else:
         return "None"
 
+
 def parse_text(s):
+    '''Parses a text by splitting up elements separated by whitespace. The elements are then
+    try to be parsed as lists of floats, ints or strings or, if only one element is found,
+    are tried to be parsed using the function parse_number().'''
     numstrings = s.split()
     if numstrings == []:
         return None
@@ -97,8 +122,9 @@ def parse_text(s):
     else:
         return parse_number(s)
 
+
 def createPrimitive(pname, ptype, psize, player = 0, pmaterial = "None", plocation = (0, 0, 0), protation = (0, 0, 0), verbose=False):
-    """Generates the primitive specified by the input parameters"""
+    '''Generates the primitive specified by the input parameters'''
     if verbose:
         print(ptype, psize)
     try:
@@ -138,6 +164,7 @@ def defLayers(layerlist):
         layers[layer] = True
     return layers
 
+
 def returnObjectList(marstype):
     """Returns list of all objects in the current scene matching marstype"""
     objlist = []
@@ -145,6 +172,7 @@ def returnObjectList(marstype):
         if obj.MARStype == marstype:
             objlist.append(obj)
     return objlist
+
 
 #TODO: this has to be tested throroughly, because it might run pretty damn long!
 def getChildren(root):
@@ -154,6 +182,7 @@ def getChildren(root):
         if getRoot(obj) == root:
             children.append(obj)
     return children
+
 
 def getImmediateChildren(obj, marstypes = ['None']):
     """Finds all immediate children for a given object"""
@@ -167,6 +196,7 @@ def getImmediateChildren(obj, marstypes = ['None']):
                 children.append(child)
     return children
 
+
 def getRoot(obj = None):
     """Finds the root object of a model given one of the model elements is selected or provided"""
     if obj == None:
@@ -177,6 +207,7 @@ def getRoot(obj = None):
     while child.parent != None:
         child = child.parent
     return child
+
 
 def getRoots():
     """Returns a list of all roots (=objects without parent) present in the scene"""
@@ -191,10 +222,12 @@ def getRoots():
         print("MARStools: Found", len(roots), "root object(s)", [root.name+"; " for root in roots])
     return roots #TODO: Should we change this and all other list return values in a tuple or generator expression?
 
+
 def calcBoundingBoxCenter(boundingbox):
     """Calculates the center of a bounding box"""
     c = sum((mathutils.Vector(b) for b in boundingbox), mathutils.Vector())
     return c / 8
+
 
 def selectObjects(objects, clear):
     """Selects all objects provided in list, clears current selection if clear==True"""
@@ -204,13 +237,18 @@ def selectObjects(objects, clear):
     for obj in objects:
         obj.select = True
 
+
 def replaceNameElement(prop, old, new):
+    '''For all selected elements in Blender, replace an *old* part of a string *prop*erty with *new*.'''
     for obj in bpy.context.selected_objects:
         if prop in obj and obj[prop].find(old) > -1:
             obj[prop] = obj[prop].replace(old, new)
 
+
 def roundVector(v, n):
+    '''Returns a mathutils.Vector with its components rounded to n digits.'''
     return round(v.x, n), round(v.y, n), round(v.z, n)
+
 
 def epsilonToZero(data, epsilon, decimals):
     """Recursively loops through a dictionary and sets all floating values
@@ -224,13 +262,16 @@ def epsilonToZero(data, epsilon, decimals):
     else: #any other type, such as string
         return data
 
+
 def datetimeFromIso(iso):
     """Accepts a date-time string in iso format and returns a datetime object."""
     return datetime(*[int(a) for a in re.split(":|-|T|\.", iso)])
 
+
 def calculateMassOfLink(link):
     objects = (getImmediateChildren(link, ['visual', 'collision']))
     return calculateMass(objects)
+
 
 def calculateMass(objects):
     mass = 0
@@ -244,9 +285,11 @@ def calculateMass(objects):
                 mass += obj['mass']
     return mass
 
+
 def distance(objects):
     v = objects[0].matrix_world.to_translation()-objects[1].matrix_world.to_translation()
     return v.length
+
 
 def outerProduct(v, u):
     '''Returns a mathutils.Matrix representing the outer product of vectors v and u.'''
@@ -254,6 +297,7 @@ def outerProduct(v, u):
     for vi in v:
         lines.append([vi * ui for ui in u])
     return mathutils.Matrix(lines)
+
 
 
 # The following function is adapted from Bret Battey's adaptation
