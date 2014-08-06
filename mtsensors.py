5a158fd26e6ab86ef156ddd6590269bfda75aa3f
Kai von Szadkowski <kavo01@dfki.de>
2014-08-06 16:45:06 +0200

    blendertools: sensors now keep a list of attached objects

--- mtsensors.py
+++ mtsensors.py
@@ -27,10 +27,68 @@ def unregister():
     print("Unregistering mtsensors...")
 
 
-
 class AddSensorOperator(Operator):
     """AddSensorOperator"""
     bl_idname = "object.mt_add_sensor"
+    bl_label = "Add/Update a sensor"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    sensor_type = StringProperty(
+        name = "sensor_type",
+        default = "",
+        description = "type of the sensor to be created")
+
+    sensor_scale = FloatProperty(
+        name = "sensor_scale",
+        default = 0.05,
+        description = "scale of the sensor visualization")
+
+    sensor_name = StringProperty(
+        name = "sensor_name",
+        default = 0.05,
+        description = "name of the sensor")
+
+    def execute(self, context):
+        location = bpy.context.scene.cursor_location
+        if self.sensor_type in mtdefs.sensorTypes:
+            if "Node" in self.sensor_type or "Joint" in self.sensor_type or "Motor" in self.sensor_type:
+                objects = []
+                sensors = []
+                for obj in bpy.context.selected_objects:
+                    if obj.MARStype == "sensor":
+                        sensors.append(obj)
+                        self.sensor_type = obj["sensorType"]
+                    else:
+                        objects.append(obj)
+                if len(sensors) <= 0:
+                    sense = mtutility.createPrimitive(self.sensor_type, "sphere", self.sensor_scale, mtdefs.layerTypes["sensor"], "sensor", location)
+                    sense.MARStype = "sensor"
+                    sense.name = self.sensor_name if self.sensor_name != '' else 'new_' + self.sensor_type
+                    sense["sensorType"] = self.sensor_type
+                    sensors.append(sense)
+                    sense.parent = mtutility.getRoot(objects[0])
+                for sensor in sensors:
+                    if "Node" in sensor["sensorType"]:
+                        sensor['nodes'] = [obj for obj in objects if obj.MARStype == 'collision']
+                    elif "Joint" in sensor["sensorType"] or "Motor" in sensor["sensorType"]:
+                        sensor['joints'] = [obj for obj in objects if obj.MARStype == 'link']
+            else: #visual sensor
+                if self.sensor_type in ["RaySensor", "RotatingRaySensor", "ScanningSonar"]:
+                    print("Added nodes to new " + self.sensor_type)
+                elif self.sensor_type == "CameraSensor":
+                    bpy.ops.object.add(type='CAMERA', location=bpy.context.scene.cursor_location)
+                    sensor = bpy.context.active_object
+                    print("Added nodes to new " + self.sensor_type)
+            for prop in mtdefs.sensorProperties[self.sensor_type]:
+                sensor[prop] = mtdefs.sensorProperties[self.sensor_type][prop]
+        else:
+            print("Error: Sensor could not be created: unknown sensor type.")
+        return {'FINISHED'}
+
+
+class AddLegacySensorOperator(Operator):
+    """AddSensorOperator"""
+    bl_idname = "object.mt_add_legacy_sensor"
     bl_label = "Add/Update a sensor"
     bl_options = {'REGISTER', 'UNDO'}
 
