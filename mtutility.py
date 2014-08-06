827b0a5ec0595b272fa1681aac69c8391d0ff6b0
Kai von Szadkowski <kavo01@dfki.de>
2014-08-06 10:46:09 +0200

    blendertools: simplified and extended selectObjects utility function

--- mtutility.py
+++ mtutility.py
@@ -224,13 +224,15 @@ def calcBoundingBoxCenter(boundingbox):
     return c / 8
 
 
-def selectObjects(objects, clear):
-    """Selects all objects provided in list, clears current selection if clear==True"""
+def selectObjects(objects, clear=True, active=-1):
+    '''Selects all objects provided in list, clears current selection if clear is True
+    and sets one of the objects the active objects if a valid index is provided.'''
     if clear:
-        for obj in bpy.context.selected_objects:
-            obj.select = False
+        bpy.ops.object.select_all(action='DESELECT')
     for obj in objects:
         obj.select = True
+    if active >= 0:
+        bpy.context.scene.objects.active = objects[active]
 
 
 def replaceNameElement(prop, old, new):
