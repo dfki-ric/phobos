f27e62933653efeb0a72ee69c15cca08340f854a
Kai von Szadkowski <kavo01@dfki.de>
2014-08-15 11:53:37 +0200

    all of oles changes to YAMLFinder.py

--- YAMLFinder.py
+++ YAMLFinder.py
@@ -1,15 +1,22 @@
 import sys
-
-if sys.version_info < (3,4):
-	print ("v")
-	exit(0)
+f = open("yamlpath.conf", "w")
+f.truncate()#Empty the file
+#if sys.version_info < (3,4):
+#	f.write("v")
+#	f.close()
+#	print("You have to use python 3.4 or greater to install the correct version of yaml")
+#	exit(0)
 
 try:
 	import yaml
 except ImportError:
-	print("i")
+	f.write("i")
+	f.close()
+	print("There was no YAML module in the current python version. Please install it with your favorite package manager")
 	exit(0)
 	
 fullpath = yaml.__file__
-print(fullpath.split("__")[0])
+f.write(fullpath.split("yaml")[0])
+f.close()
+print("YAMl module found!")
 exit(1)
