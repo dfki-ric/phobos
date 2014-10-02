2b2620e48c609045d2ee7ed05b6c7573113563c2
Kai von Szadkowski <kavo01@dfki.de>
2014-10-02 11:33:29 +0200

    reformat utility

--- utility.py
+++ utility.py
@@ -29,7 +29,6 @@ import re
 import bpy
 import mathutils
 from datetime import datetime
-from . import materials
 from . import defs
 
 
@@ -42,15 +41,16 @@ def unregister():
 
 
 def is_float(s):
-    '''Tests if an input variable (string) is a float number.'''
+    """Tests if an input variable (string) is a float number."""
     try:
         float(s)
         return True
     except (ValueError, TypeError):
         return False
 
+
 def is_int(s):
-    '''Tests is an input variable (string) is an int number.'''
+    """Tests is an input variable (string) is an int number."""
     try:
         int(s)
         return True
@@ -59,8 +59,8 @@ def is_int(s):
 
 
 def parse_number(s):
-    '''Takes an input variable (string) and determines whether it represents
-    a float number, int or string'''
+    """Takes an input variable (string) and determines whether it represents
+    a float number, int or string"""
     if is_int(s):
         return int(s)
     elif is_float(s):
@@ -70,7 +70,7 @@ def parse_number(s):
 
 
 def only_contains_int(stringlist):
-    '''Checks if a list of strings contains int numbers exclusively.'''
+    """Checks if a list of strings contains int numbers exclusively."""
     for num in stringlist:
         if not is_int(num):
             return False
@@ -78,7 +78,7 @@ def only_contains_int(stringlist):
 
 
 def only_contains_float(stringlist):
-    '''Checks if a list of strings contains float numbers exclusively.'''
+    """Checks if a list of strings contains float numbers exclusively."""
     for num in stringlist:
         if not is_float(num):
             return False
@@ -86,8 +86,8 @@ def only_contains_float(stringlist):
 
 
 def find_in_list(alist, prop, value):
-    '''Returns the index of the first object in a list which has a field
-    named *prop* with value *value*. If no such object is found, returns -1.'''
+    """Returns the index of the first object in a list which has a field
+    named *prop* with value *value*. If no such object is found, returns -1."""
     n = -1
     for i in range(len(alist)):
         try:
@@ -100,8 +100,8 @@ def find_in_list(alist, prop, value):
 
 
 def retrieve_from_list(alist, prop, value):
-    '''Returns the first object in a list which has a field named
-    *prop* with value *value*. If no such object is found, returns "None".'''
+    """Returns the first object in a list which has a field named
+    *prop* with value *value*. If no such object is found, returns "None"."""
     n = -1
     for i in range(len(alist)):
         try:
@@ -117,9 +117,9 @@ def retrieve_from_list(alist, prop, value):
 
 
 def parse_text(s):
-    '''Parses a text by splitting up elements separated by whitespace. The elements are then
+    """Parses a text by splitting up elements separated by whitespace. The elements are then
     try to be parsed as lists of floats, ints or strings or, if only one element is found,
-    are tried to be parsed using the function parse_number().'''
+    are tried to be parsed using the function parse_number()."""
     numstrings = s.split()
     if numstrings == []:
         return None
@@ -131,7 +131,7 @@ def parse_text(s):
             nums = [float(num) for num in numstrings]
             return nums
         else:
-            return numstrings #s
+            return numstrings  # s
     else:
         return parse_number(s)
 
@@ -144,27 +144,30 @@ def printMatrices(info, obj):
           "\n\nbasis:\n", obj.matrix_basis)
 
 
-def createPrimitive(pname, ptype, psize, player = 0, pmaterial = "None", plocation = (0, 0, 0), protation = (0, 0, 0), verbose=False):
-    '''Generates the primitive specified by the input parameters'''
+def createPrimitive(pname, ptype, psize, player=0, pmaterial="None", plocation=(0, 0, 0), protation=(0, 0, 0),
+                    verbose=False):
+    """Generates the primitive specified by the input parameters"""
     if verbose:
         print(ptype, psize)
     try:
-        #n_layer = bpy.context.scene.active_layer
+        # n_layer = bpy.context.scene.active_layer
         n_layer = int(player)
     except ValueError:
         n_layer = defs.layerTypes[player]
     players = defLayers([n_layer])
-    bpy.context.scene.layers[n_layer] = True #the layer has to be active to prevent problems with object placement
+    bpy.context.scene.layers[n_layer] = True  # the layer has to be active to prevent problems with object placement
     if ptype == "box":
-        bpy.ops.mesh.primitive_cube_add(layers = players, location = plocation, rotation = protation)
+        bpy.ops.mesh.primitive_cube_add(layers=players, location=plocation, rotation=protation)
         obj = bpy.context.object
         obj.dimensions = psize
     if ptype == "sphere":
-        bpy.ops.mesh.primitive_uv_sphere_add(size = psize, layers = players, location = plocation, rotation = protation)
+        bpy.ops.mesh.primitive_uv_sphere_add(size=psize, layers=players, location=plocation, rotation=protation)
     elif ptype == "cylinder":
-        bpy.ops.mesh.primitive_cylinder_add(vertices=32, radius=psize[0], depth=psize[1], layers=players, location = plocation, rotation = protation)
+        bpy.ops.mesh.primitive_cylinder_add(vertices=32, radius=psize[0], depth=psize[1], layers=players,
+                                            location=plocation, rotation=protation)
     elif ptype == "cone":
-        bpy.ops.mesh.primitive_cone_add(vertices=32,radius=psize[0],depth=psize[1], cap_end=True, layers=players, location=plocation, rotation = protation)
+        bpy.ops.mesh.primitive_cone_add(vertices=32, radius=psize[0], depth=psize[1], cap_end=True, layers=players,
+                                        location=plocation, rotation=protation)
     obj = bpy.context.object
     obj.name = pname
     if pmaterial != 'None':
@@ -175,7 +178,7 @@ def createPrimitive(pname, ptype, psize, player = 0, pmaterial = "None", plocati
 
 def defLayers(layerlist):
     """Returns a list of 20 elements encoding the visible layers according to layerlist"""
-    layers = 20*[False]
+    layers = 20 * [False]
     for layer in layerlist:
         layers[layer] = True
     return layers
@@ -193,16 +196,16 @@ def returnObjectList(marstype):
 def getChildren(root):
     """Finds all children for a given root"""
     children = []
-    for obj in bpy.data.objects: #TODO: this is not the best list to iterate over (there might be multiple scenes)
+    for obj in bpy.data.objects:  # TODO: this is not the best list to iterate over (there might be multiple scenes)
         if getRoot(obj) == root:
             children.append(obj)
     return children
 
 
-def getImmediateChildren(obj, marstypes = ['None']):
+def getImmediateChildren(obj, marstypes=['None']):
     """Finds all immediate children for a given object"""
     children = []
-    for child in bpy.data.objects: #TODO: this is not the best list to iterate over (there might be multiple scenes)
+    for child in bpy.data.objects:  # TODO: this is not the best list to iterate over (there might be multiple scenes)
         if child.parent == obj:
             if marstypes != ['None']:
                 if child.MARStype in marstypes:
@@ -212,10 +215,10 @@ def getImmediateChildren(obj, marstypes = ['None']):
     return children
 
 
-def getRoot(obj = None):
+def getRoot(obj=None):
     """Finds the root object of a model given one of the model elements is selected or provided"""
     if obj == None:
-        for anobj in bpy.data.objects: #TODO: this is not the best list to iterate over (there might be multiple scenes)
+        for anobj in bpy.data.objects:  # TODO: this is not the best list to iterate over (there might be multiple scenes)
             if (anobj.select):
                 obj = anobj
     child = obj
@@ -227,15 +230,15 @@ def getRoot(obj = None):
 def getRoots():
     """Returns a list of all roots (=objects without parent) present in the scene"""
     roots = []
-    for obj in bpy.data.objects: #TODO: this is not the best list to iterate over (there might be multiple scenes)
+    for obj in bpy.data.objects:  # TODO: this is not the best list to iterate over (there might be multiple scenes)
         if not obj.parent and obj.MARStype == "link":
             roots.append(obj)
 
     if roots == []:
         print("MARStools: No root objects found.")
     else:
-        print("MARStools: Found", len(roots), "root object(s)", [root.name+"; " for root in roots])
-    return roots #TODO: Should we change this and all other list return values in a tuple or generator expression?
+        print("MARStools: Found", len(roots), "root object(s)", [root.name + "; " for root in roots])
+    return roots  # TODO: Should we change this and all other list return values in a tuple or generator expression?
 
 
 def calcBoundingBoxCenter(boundingbox):
@@ -256,14 +259,14 @@ def selectObjects(objects, clear=True, active=-1):
 
 
 def replaceNameElement(prop, old, new):
-    '''For all selected elements in Blender, replace an *old* part of a string *prop*erty with *new*.'''
+    """For all selected elements in Blender, replace an *old* part of a string *prop*erty with *new*."""
     for obj in bpy.context.selected_objects:
         if prop in obj and obj[prop].find(old) > -1:
             obj[prop] = obj[prop].replace(old, new)
 
 
 def roundVector(v, n):
-    '''Returns a mathutils.Vector with its components rounded to n digits.'''
+    """Returns a mathutils.Vector with its components rounded to n digits."""
     return round(v.x, n), round(v.y, n), round(v.z, n)
 
 
@@ -276,12 +279,12 @@ def epsilonToZero(data, epsilon, decimals):
         return [epsilonToZero(a, epsilon, decimals) for a in data]
     elif type(data) is dict:
         return {key: epsilonToZero(value, epsilon, decimals) for key, value in data.items()}
-    else: #any other type, such as string
+    else:  # any other type, such as string
         return data
 
 
 def calculateSum(objects, numeric_prop):
-    '''Returns sum of *numeric_prop* in *objects*.'''
+    """Returns sum of *numeric_prop* in *objects*."""
     numsum = 0
     for obj in objects:
         try:
@@ -297,19 +300,19 @@ def datetimeFromIso(iso):
 
 
 def distance(objects):
-    v = objects[0].matrix_world.to_translation()-objects[1].matrix_world.to_translation()
+    v = objects[0].matrix_world.to_translation() - objects[1].matrix_world.to_translation()
     return v.length, v
 
 
 def outerProduct(v, u):
-    '''Returns a mathutils.Matrix representing the outer product of vectors v and u.'''
+    """Returns a mathutils.Matrix representing the outer product of vectors v and u."""
     lines = []
     for vi in v:
         lines.append([vi * ui for ui in u])
     return mathutils.Matrix(lines)
 
 
-#def useLegacyNames(data):
+# def useLegacyNames(data):
 #    if type(data) is str:
 #        print(data, end=': ')
 #        if data in defs.MARSlegacydict:
