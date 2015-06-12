ec9b903597133748396d4c1bc9429615b5500c3e
Ole Schwiegert <ole.schwiegert@uni-bremen.de>
2015-06-12 11:23:45 +0200

    Added general module

---
+++ src/utils/general.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+"""
+.. module:: phobos.utils.general
+    :platform: Unix, Windows, Mac
+    :synopsis: This module contains general functions to use in operators and custom scripts
+
+.. moduleauthor:: Kai von Szadowski, Ole Schwiegert
+
+Copyright 2014, University of Bremen & DFKI GmbH Robotics Innovation Center
+
+This file is part of Phobos, a Blender Add-On to edit robot models.
+
+Phobos is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation, either version 3
+of the License, or (at your option) any later version.
+
+Phobos is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Phobos.  If not, see <http://www.gnu.org/licenses/>.
+"""
