847005671d392fc5c0133d2f85f82c9d2d31f36e
Ole Schwiegert <ole.schwiegert@uni-bremen.de>
2015-09-29 09:49:17 +0200

    Add heightmap mesh export implementation

--- src/exporter.py
+++ src/exporter.py
@@ -1132,12 +1132,44 @@ def handleScene_heightmap(heightmap, outpath, subfolder):
     log("Exporting " + heightmap["entityname"] + " as a heightmap entity", "INFO")
     entry = {}
     entitypose = robotdictionary.deriveObjectPose(heightmap)
+    heightmapMesh = selectionUtils.getImmediateChildren(heightmap)[0]
     if bpy.data.worlds[0].heightmapMesh:
-        log("Heightmap as mesh is not implemented yet. Skipping heightmap", "ERROR")
+        exMesh = heightmapMesh.to_mesh(bpy.context.scene, True, "PREVIEW")
+        exMesh.name = "hm_" + heightmap["entityname"]
+        oldMesh = heightmapMesh.data
+        heightmapMesh.data = exMesh
+        heightmapMesh.modifiers["displace_heightmap"].show_render = False
+        heightmapMesh.modifiers["displace_heightmap"].show_viewport = False
+        if bpy.data.worlds[0].useObj:
+            exportObj(outpath, heightmapMesh)
+            filename = os.path.join("heightmaps", exMesh.name + ".obj")
+        elif bpy.data.worlds[0].useBobj:
+            exportBobj(outpath, heightmapMesh)
+            filename = os.path.join("heightmaps", exMesh.name + ".bobj")
+        elif bpy.data.worlds[0].useStl:
+            exportStl(outpath, heightmapMesh)
+            filename = os.path.join("heightmaps", exMesh.name + ".stl")
+        elif bpy.data.worlds[0].useDae:
+            exportDae(outpath, heightmapMesh)
+            filename = os.path.join("heightmaps", exMesh.name + ".dae")
+        else:
+            log("No mesh export type checked! Aborting heightmap export.", "ERROR", __name__+".handleScene_heightmap")
+            return {}
+        heightmapMesh.modifiers["displace_heightmap"].show_render = True
+        heightmapMesh.modifiers["displace_heightmap"].show_viewport = True
+        heightmapMesh.data = oldMesh
+        bpy.data.meshes.remove(exMesh)
+        entry = {"name": heightmap["entityname"],
+                "type": "mesh",
+                "file": filename,
+                "anchor": heightmap["anchor"] if "anchor" in heightmap else "none",
+                "position": entitypose["translation"],
+                "rotation": entitypose["rotation_quaternion"]
+                }
+
     else:
         imagepath = os.path.abspath(os.path.join(os.path.split(bpy.data.filepath)[0], heightmap["image"]))
         shutil.copy2(imagepath, outpath)
-        heightmapMesh = selectionUtils.getImmediateChildren(heightmap)[0]
         entry = {"name": heightmap["entityname"],
                 "type": "heightmap",
                 "file": os.path.join("heightmaps", os.path.basename(imagepath)),
