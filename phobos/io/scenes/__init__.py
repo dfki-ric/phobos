37c6eb5e051597e863b4743b9f0b2ab6c26c2ecc
Kai von Szadkowski <kai.von-szadkowski@uni-bremen.de>
2016-12-08 15:52:02 +0100

    Adapt scene export to generic setup

---
+++ phobos/io/scenes/__init__.py
@@ -0,0 +1,13 @@
+import os
+import imp
+
+scene_types = dict()
+structure_export_folders = []
+for filename in os.listdir(os.path.dirname(__file__)):
+    mod_name, file_ext = os.path.splitext(os.path.split(filename)[-1])
+    if (filename != os.path.split(__file__)[-1]) and (file_ext.lower() == '.py'):
+        py_mod = imp.load_source(mod_name, os.path.join(os.path.dirname(__file__), filename))
+
+        if hasattr(py_mod, 'mesh_type_dict'):
+            # mesh_types = {**mesh_types, **py_mod.mesh_type_dict}  # unpacking (> Python 3.5)
+            scene_types.update(py_mod.mesh_type_dict.copy())
