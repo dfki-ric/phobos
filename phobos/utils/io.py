8dfe215c7168facd61682275628dbe8f69d4c750
Kai von Szadkowski <kai.von-szadkowski@uni-bremen.de>
2016-12-08 16:11:43 +0100

    Revise code documentation of utils/io module

--- phobos/utils/io.py
+++ phobos/utils/io.py
@@ -11,19 +11,19 @@ xmlHeader = '<?xml version="1.0"?>\n<!-- created with Phobos ' + defs.version + 
 
 
 def xmlline(ind, tag, names, values):
-    """This function generates an xml line with specified values.
+    """Generates an xml line with specified values.
     To use this function you need to know the indentation level you need for this line.
     Make sure the names and values list have the correct order.
 
-    :param ind: The level of indentation
-    :type ind: int, has to be positive!
-    :param tag: This is the xml lines tag
+    :param ind: Indentation level
+    :type ind: int >= 0
+    :param tag: xml element tag
     :type tag: String
-    :param names: This are the names of the xml lines attributions.
-    :type names: list, check for analogue order to values.
-    :param values: This are the values of the xml lines attributions.
-    :type values: list, check  for analogue order to names.
-    :return: String -- the generated xml line.
+    :param names: Names of xml element's attributes
+    :type names: list (same order as for values)
+    :param values: Values of xml element's attributes
+    :type values: list (same order as for names)
+    :return: String -- Generated xml line.
 
     """
     line = [indent * max(0, ind) + '<' + tag]
@@ -34,16 +34,15 @@ def xmlline(ind, tag, names, values):
 
 
 def l2str(items, start=0, end=-1):
-    """This function takes a list and generates a String with its element.
+    """Generates string from (part of) a list.
 
-    :param items: The list of elements you want to generate a String from. *Make sure the elements can be cast to
-    Strings with str().*
+    :param items: List from which the string is derived (elements need to implement str())
     :type items: list
-    :param start: The inclusive start index to iterate the list from. If negative it defaults to 0.
+    :param start: Inclusive start index for iteration
     :type start: int
-    :param end: The exclusive end to iterate the list to. If negative its defaults to len(items).
+    :param end: Exclusive end index for iteration
     :type end: int
-    :return: str - the generated string.
+    :return: str - Generated string.
 
     """
     start = max(start, 0)
@@ -52,12 +51,11 @@ def l2str(items, start=0, end=-1):
 
 
 def securepath(path):
-    """This function checks whether a path exists or not.
-    If it doesn't the functions creates the path.
+    """Checks whether directories of a path exist and generates them if necessary.
 
-    :param path: The path you want to check for existence *DIRECTORIES ONLY*
+    :param path: The path to be secured (directories only)
     :type path: str
-    :return: String -- the path given as parameter, but secured by expanding ~ constructs.
+    :return: String -- secured path as absolute path, None on error
 
     """
     path = os.path.abspath(path)
@@ -72,6 +70,9 @@ def securepath(path):
 
 
 def getgitbranch():
+    """Checks whether working directory (of .blend file) contains a git repository.
+    Returns branch if repository is found.
+    """
     try:
         output = str(subprocess.check_output(['git', 'branch'], cwd=bpy.path.abspath('//'), universal_newlines=True))
         branch = [a for a in output.split('\n') if a.find('*') >= 0][0]
@@ -80,3 +81,4 @@ def getgitbranch():
         return None
     except FileNotFoundError:
         log("No git repository found.", "ERROR", origin="utils/io/getgitbranch")
+        return None
