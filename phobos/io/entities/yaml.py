5b7aa2c83d2d9e0c10e87e9a34912b8bb54baa36
Kai von Szadkowski <kai.von-szadkowski@uni-bremen.de>
2017-01-30 12:47:05 +0100

    Adapt YAML export to new format

--- phobos/io/entities/yaml.py
+++ phobos/io/entities/yaml.py
@@ -1,16 +1,29 @@
-def exportModelToYAML(model, filepath):
+import yaml
+import os
+from datetime import datetime
+import phobos.defs as defs
+from phobos.phoboslog import log
+
+
+def exportYAML(model, path, mesh_format=''):
     """This function exports a given robot model to a specified filepath as YAML.
 
     :param model: The robot model to export
     :type model: dict -- the generated robot model dictionary
-    :param filepath:  The filepath to export the robot to. *WITH filename!*
-    :type filepath: str
+    :param path:  The filepath to export the robot to. *WITH filename!*
+    :type path: str
 
     """
-    log("phobos YAML export: Writing model data to " + filepath, "INFO", "exportModelToYAML")
-    with open(filepath, 'w') as outputfile:
+    log("phobos YAML export: Writing model data to " + path, "INFO", "exportModelToYAML")
+    with open(os.path.join(path, model['name'], '.yaml'), 'w') as outputfile:
         outputfile.write('# YAML dump of robot model "' + model['name'] + '", ' + datetime.now().strftime(
             "%Y%m%d_%H:%M") + "\n")
         outputfile.write("# created with Phobos" + defs.version + " - https://github.com/rock-simulation/phobos\n\n")
         outputfile.write(yaml.dump(
             model))  # default_flow_style=False)) #last parameter prevents inline formatting for lists and dictionaries
+
+
+# registering export functions of types with Phobos
+entity_type_dict = {'yaml': {'export': exportYAML,
+                             'extensions': ('yaml', 'yml')}
+                    }
