0a34c1a9538ab33fff261659b3670ebf01f8a530
Kai von Szadkowski <kai.von-szadkowski@uni-bremen.de>
2017-01-30 12:52:42 +0100

    Unify link creation

--- phobos/model/links.py
+++ phobos/model/links.py
@@ -31,89 +31,74 @@ Created on 14 Apr 2014
 """
 
 import bpy
+import mathutils
 import math
 import phobos.defs as defs
 import phobos.utils.naming as nUtils
 import phobos.utils.blender as bUtils
 import phobos.utils.selection as sUtils
+import phobos.model.inertia as inertiamodel
+import phobos.model.geometries as geometrymodel
 from phobos.phoboslog import log
 
 
-def createLink(self, link):
-        """This function creates the blender representation of a given link
-
-        :param link: The link you want to create a representation of.
-        :type link: dict
-        :return: bpy_types.Object -- the newly created blender link object.
-
-        """
-        bpy.context.scene.layers = bUtils.defLayers(defs.layerTypes['link'])
-        #create base object ( =armature)
-        bpy.ops.object.select_all(action='DESELECT')
-        #bpy.ops.view3d.snap_cursor_to_center()
-        bpy.ops.object.armature_add(layers=bUtils.defLayers(0))
-        newlink = bpy.context.active_object #print(bpy.context.object) #print(bpy.context.scene.objects.active) #bpy.context.selected_objects[0]
-        newlink["link/name"] = link['name']
-        newlink.name = self.praefixNames(link['name'], "link")
-        #newlink.location = (0.0, 0.0, 0.0)
-        newlink.location = link['pose']['translation']
-        newlink.scale = (0.3, 0.3, 0.3) #TODO: make this depend on the largest visual or collision object
-        bpy.ops.object.transform_apply(scale=True)
-        newlink.phobostype = 'link'
-        if newlink.name != self.praefixNames(link['name'], "link"):
-            log("Warning, name conflict!")
-        # place inertial
-        if 'inertial' in link:
-            self.createInertial(link['name'], link['inertial'])
-        # place visual
-        if 'visual' in link:
-            for v in link['visual']:
-                visual = link['visual'][v]
-                if 'geometry' in visual:
-                    self.createGeometry(visual, 'visual')
-        # place collision
-        if 'collision' in link:
-            for c in link['collision']:
-                collision = link['collision'][c]
-                if 'geometry' in collision:
-                    self.createGeometry(collision, 'collision')
-        for prop in link:
-            if prop.startswith('$'):
-                for tag in link[prop]:
-                    newlink['link/'+prop[1:]+'/'+tag] = link[prop][tag]
-        return newlink
+def getGeometricElements(link):
+    visuals = []
+    collisions = []
+    if 'visual' in link:
+        visuals = [link['visual'][v] for v in link['visual']]
+    if 'collision' in link:
+        collisions = [link['collision'][v] for v in link['collision']]
+    return visuals + collisions
 
 
-def createLink(scale, position=None, orientation=None, name=''):
-    """Creates an empty link (bone) at the current 3D cursor position.
+def createLink(link):
+    """Creates the blender representation of a given link and its parent joint.
 
-    :param scale: This is the scale you want to apply to the new link.
-    :type scale: Float array with 3 elements.
-    :param position: This specifies the position of the newly created link. When not given its (0.0,0.0,0.0)
-    :type position: Float array with 3 elements.
-    :param orientation: This specifies the rotation of the newly created link. When not given its (0.0,0.0,0.0)
-    :type orientation:Float array with 3 elements.
-    :param name: This sets the name for the new link. When not given the link is nameless.
-    :type name: str
-    :return: bpy_types.Object
+    :param link: The link you want to create a representation of.
+    :type link: dict
+    :return: bpy_types.Object -- the newly created blender link object.
 
     """
+    # create armature/bone
     bUtils.toggleLayer(defs.layerTypes['link'], True)
-    if position is None and orientation is None:
-        bpy.ops.object.armature_add(layers=bUtils.defLayers([0]))
-    elif position is None:
-        bpy.ops.object.armature_add(rotation=orientation, layers=bUtils.defLayers([0]))
-    elif orientation is None:
-        bpy.ops.object.armature_add(location=position, layers=bUtils.defLayers([0]))
-    else:
-        bpy.ops.object.armature_add(location=position, rotation=orientation, layers=bUtils.defLayers([0]))
-    link = bpy.context.active_object
-    link.scale = [scale, scale, scale]
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.ops.object.armature_add(layers=bUtils.defLayers([defs.layerTypes['link']]))
+    newlink = bpy.context.active_object
+    newlink.phobostype = 'link'
+    newlink.name = link['name']
+    # FIXME: This is a hack and should be checked before creation!
+    newlink["link/name"] = link['name']  # this is a backup in case an object with the link's name already exists
+
+    # set the size of the link
+    elements = getGeometricElements(link)
+    scale = 2 * max((geometrymodel.getLargestDimension(element['geometry']) for element in elements)) if elements else 0.2
+    newlink.scale = (scale, scale, scale)
     bpy.ops.object.transform_apply(scale=True)
-    if name:
-        link.name = name
-    link.phobostype = 'link'
-    return link
+
+    # create inertial
+    if 'inertial' in link:
+        inertiamodel.createInertialFromDictionary(link['name'], link['inertial'])
+
+    # create visual elements
+    if 'visual' in link:
+        for v in link['visual']:
+            visual = link['visual'][v]
+            geometrymodel.createGeometry(visual, 'visual')
+
+    # create collision elements
+    if 'collision' in link:
+        for c in link['collision']:
+            collision = link['collision'][c]
+            geometrymodel.createGeometry(collision, 'collision')
+
+    # add custom properties
+    for prop in link:
+        if prop.startswith('$'):
+            for tag in link[prop]:
+                newlink['link/'+prop[1:]+'/'+tag] = link[prop][tag]
+
+    return newlink
 
 
 def deriveLinkfromObject(obj, scale=0.2, parenting=True, parentobjects=False, namepartindices=[], separator='_', prefix='link'):
@@ -166,3 +151,75 @@ def deriveLinkfromObject(obj, scale=0.2, parenting=True, parentobjects=False, na
             bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
             sUtils.selectObjects([child, link], True, 1)
             bpy.ops.object.parent_set(type='BONE_RELATIVE')
+
+
+def placeChildLinks(model, parent):
+    """Creates parent-child-relationship for a given parent and all existing children in Blender.
+
+    :param parent: This is the parent link you want to set the children for.
+    :type: dict
+
+    """
+    bpy.context.scene.layers = bUtils.defLayers(defs.layerTypes['link'])
+    children = []
+    for l in model['links']:
+        if 'parent' in model['links'][l] and model['links'][l]['parent'] == parent['name']:
+            children.append(model['links'][l])
+    for child in children:
+        # 1: set parent relationship (this makes the parent inverse the inverse of the parents world transform)
+        parentLink = bpy.data.objects[parent['name']]
+        childLink = bpy.data.objects[child['name']]
+        sUtils.selectObjects([childLink, parentLink], True, 1)
+        bpy.ops.object.parent_set(type='BONE_RELATIVE')
+        # 2: move to parents origin by setting the world matrix to the parents world matrix
+        childLink.matrix_world = parentLink.matrix_world        # removing this line does not seem to make a difference
+
+        # #bpy.context.scene.objects.active = childLink
+        # if 'pivot' in child:
+        #     pivot = child['pivot']
+        #     cursor_location = bpy.context.scene.cursor_location
+        #     bpy.context.scene.cursor_location = mathutils.Vector((-pivot[0]*0.3, -pivot[1]*0.3, -pivot[2]*0.3))
+        #     bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+        #     bpy.context.scene.cursor_location = cursor_location
+
+        # 3: apply local transform as saved in model (changes matrix_local)
+        location = mathutils.Matrix.Translation(child['pose']['translation'])
+        rotation = mathutils.Euler(tuple(child['pose']['rotation_euler']), 'XYZ').to_matrix().to_4x4()
+        transform_matrix = location * rotation
+        childLink.matrix_local = transform_matrix
+        # 4: be happy, as world and basis are now the same and local is the transform to be exported to urdf
+        # 5: take care of the rest of the tree
+        placeChildLinks(child)
+
+
+def placeLinkSubelements(link):
+    """Finds all subelements for a given link and sets the appropriate relations.
+    In this case subelements are interials, visuals and collisions.
+
+    :param link: The parent link you want to set the subelements for
+    :type link: dict
+
+    """
+    elements = getGeometricElements(link) + link['inertial'] if 'inertial' in link else None
+    bpy.context.scene.layers = bUtils.defLayers([defs.layerTypes[t] for t in defs.layerTypes])
+    parentlink = bpy.data.objects[link['name']]
+    for element in elements:
+        if 'pose' in element:
+            location = mathutils.Matrix.Translation(element['pose']['translation'])
+            rotation = mathutils.Euler(tuple(element['pose']['rotation_euler']), 'XYZ').to_matrix().to_4x4()
+        else:
+            location = mathutils.Matrix.Identity(4)
+            rotation = mathutils.Matrix.Identity(4)
+        try:
+            obj = bpy.data.objects[element['name']]
+        except KeyError:
+            log('Missing link element for placement: ' + element['name'], 'ERROR', 'placeLinkSubelements')
+            continue
+        sUtils.selectObjects([obj, parentlink], True, 1)
+        bpy.ops.object.parent_set(type='BONE_RELATIVE')
+        obj.matrix_local = location * rotation
+        try:
+            element.scale = element['geometry']['scale']
+        except KeyError:
+            pass
+        # sUtils.selectObjects([element, parentlink], True, 1)
