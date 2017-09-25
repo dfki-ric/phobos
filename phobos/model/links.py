f769569dafb1ab2c4a0ad2f0f471557652240cfa
Kai von Szadkowski <kai.von-szadkowski@uni-bremen.de>
2017-09-25 13:39:34 +0200

    Fix bug of getGeometricElements not returning collision objects
    "collisions and visuals" - > "collisions + visuals", simplified with
    different initialization of the two variables
    Interestingly, Python turns [1, 2] and [3, 4] into [3, 4] instead of
    True, thus this function failed silently.
    
    Conflicts:
    	phobos/model/links.py

--- phobos/model/links.py
+++ phobos/model/links.py
@@ -43,22 +43,14 @@ from phobos.phoboslog import log
 
 
 def getGeometricElements(link):
-    visuals = False
-    collisions = False
+    # DOCU add some docstring
+    visuals = []
+    collisions = []
     if 'visual' in link:
         visuals = [link['visual'][v] for v in link['visual']]
     if 'collision' in link:
         collisions = [link['collision'][v] for v in link['collision']]
-
-    # avoid uninitialized variables from manually created links
-    if (visuals and not collisions):
-        return visuals
-    elif (collisions and not visuals):
-        return collisions
-    elif (collisions and visuals):
-        return collisions and visuals
-    else:
-        return None
+    return collisions + visuals
 
 def createLink(link):
     """Creates the blender representation of a given link and its parent joint.
