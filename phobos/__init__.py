7b709bb48b9e45ec334270c0ea7a6607d3df41f0
Simon Reichel <simon.reichel@dfki.de>
2017-11-15 15:34:14 +0100

    Bugfix of failing definition parsing
    The definitions of Phobos are parsed from the definitions folder.
    However, this failed silently, as the defs.py contains only a relative
    link to this folder, which was not found.
    
    Move the definition folder to its proper place, so it will be copied
    accordingly by the installation script.
    
    Another issue was caused by the string formatting in the Addon bl_info.

--- phobos/__init__.py
+++ phobos/__init__.py
@@ -82,10 +82,8 @@ def import_submodules(package, recursive=True, verbose=False):
 bl_info = {
     "name": "Phobos",
     "description": "A toolbox to enable editing of robot models in Blender.",
-    "author": "Kai von Szadkowski, Ole Schwiegert, Stefan Rahms, " +
-                "Malte Langosz, Simon Reichel",
-    "version": (0, 7, 2),
-    # DOCU update Blender version @GUI (30)
+    "author": "Kai von Szadkowski, Ole Schwiegert, Stefan Rahms, Malte Langosz, Simon Reichel",
+    "version": (0, 7, 1),
     "blender": (2, 69, 0),
     "location": "Phobos adds a number of custom tool panels.",
     "warning": "",
@@ -101,16 +99,14 @@ if os.path.isfile(yamlconfpath):
     f = open(yamlconfpath)
     path = f.read()
     f.close()
+    # CHECK is this if still required?
     if path == "v" or path == "i":
         print("There is no YAML installation for python 3.4 or greater on this computer")
     else:
-        sys.path.insert(0, path)
+        sys.path.insert(0, path.replace('/yaml', ''))
         import yaml
 else:
-    print("Could not find yamlpath.conf")
-    print("Using distributed package instead!")
-    sys.path.insert(0, sys.path[0] + "/phobos")
-    import yaml
+    print('No yamlpath.conf file found. Please reinstall phobos.')
 
 
 # Add custom YAML (de-)serializer
