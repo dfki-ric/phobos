f205f705310a08eaec41d30724fa7ee4de7c3835
Simon Reichel <simon.reichel@dfki.de>
2018-02-28 17:12:16 +0100

    Fix entity, library, meshes and scenes initialisation
    The plugin initialisations were using a python module (imp) which was
    deprecated since version 3.4.
    This might introduce errors, as Blender updates its API.
    
    The required functions were replaced with a proper call to importlib.
    (see https://docs.python.org/3.5/library/imp.html and
    https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path#67692)
    
    The existing try-catch blocks were replaced with a unified check for
    existing attributes.
    
    The documentation was updated on the fly and a package docstring was
    added.

--- phobos/io/libraries/__init__.py
+++ phobos/io/libraries/__init__.py
@@ -1,13 +1,56 @@
+#!/usr/bin/python3.5
+# coding=utf-8
+
+"""
+..module:: phobos.io.libraries
+    :platform: Unix, Windows, Mac
+    :synopsis: TODO: INSERT TEXT HERE
+
+..moduleauthor:: Kai von Szadowski, Ole Schwiegert, Stefan Rahms, Malte Langosz, Sebastian Klemp, Simon Reichel
+
+Copyright 2017, University of Bremen & DFKI GmbH Robotics Innovation Center
+
+This file is part of Phobos, a Blender Add-On to edit robot models.
+
+Phobos is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation, either version 3
+of the License, or (at your option) any later version.
+
+Phobos is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Phobos.  If not, see <http://www.gnu.org/licenses/>.
+
+File phobos.io.libraries.__init__.py
+"""
+
 import os
-import imp
+import importlib.util
 
 def register():
+    ''' Register the library plugins by calling each individual register function
+    for every python file in this folder. '''
+    # this loops over every python file in this subfolder
     for filename in os.listdir(os.path.dirname(__file__)):
         mod_name, file_ext = os.path.splitext(os.path.split(filename)[-1])
+
+        # only take .py files and ignore this __init__ file
         if (filename != os.path.split(__file__)[-1]) and (file_ext.lower() == '.py'):
-            py_mod = imp.load_source(mod_name, os.path.join(os.path.dirname(__file__), filename))
-            #try:
-            py_mod.register()
-            print('Registering phobos lib plugin:', mod_name)
-            #except AttributeError:
-            #    print('ERROR in libraries/__init__: "'+mod_name+'" has no register function.')
+            modpath = os.path.join(os.path.dirname(__file__), filename)
+
+            # load the module from file and source it to access its data
+            spec = importlib.util.spec_from_file_location(filename, modpath)
+            py_mod = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(py_mod)
+
+            # try to register the submodule
+            if hasattr(py_mod, 'register'):
+                py_mod.register()
+                print('Registered phobos library plugin:', mod_name)
+            else:
+                print('ERROR in libraries/__init__: "' + mod_name +
+                      '" has no register function.')
