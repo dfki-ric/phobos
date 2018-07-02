66337d6dea6629755b9423e5e40490b02d021aa6
Simon Reichel <simms1@web.de>
2018-07-02 15:18:29 +0200

    Fix sphinx conf.py
    We need to import bpy in the configfile

--- doc/conf.py
+++ doc/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+import bpy
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
