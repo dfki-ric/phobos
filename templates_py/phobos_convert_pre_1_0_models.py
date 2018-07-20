1509b10eb766a3c5f11f247620a13f4fc62243a2
Kai von Szadkowski <kai.von-szadkowski@uni-bremen.de>
2018-07-20 11:50:02 +0200

    Add conversion script for pre-1.0-Models

---
+++ templates_py/phobos_convert_pre_1_0_models.py
@@ -0,0 +1,27 @@
+import bpy
+from phobos.utils.selection import selectObjects
+
+# -----------------------------------------------------------------
+# configuration
+# -----------------------------------------------------------------
+# remove existing link inertial objects? (no longer required if derived from helper objects)
+delete_link_inertials = True
+# convert only selected objects or all objects in .blend file?
+selected_only = False
+# -----------------------------------------------------------------
+
+objects_to_be_deleted = []
+objectlist = bpy.context.selected_objects if selected_only else bpy.data.objects
+for obj in objectlist:
+    if obj.phobostype == 'inertial' and 'mass' in obj and 'inertia' in obj:
+        obj['inertial/mass'] = obj['mass']
+        del obj['mass']
+        obj['inertial/inertia'] = obj['inertia']
+        del obj['inertia']
+    elif (obj.phobostype == 'inertial' and 'inertial/mass' in obj and 'inertial/inertia' in obj
+          and delete_link_inertials):
+        objects_to_be_deleted.append(obj)
+
+# delete objects designated for deletion
+selectObjects(objects_to_be_deleted)
+bpy.ops.object.delete(use_global=True)
