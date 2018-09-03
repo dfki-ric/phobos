365cd72497ebab2d1b4bb3d24bdb875c2d61a0c7
Simon Reichel <simms1@web.de>
2018-09-03 16:52:24 +0200

    Add file headers to io files

--- phobos/io/scenes/smurfs.py
+++ phobos/io/scenes/smurfs.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python
 # coding=utf-8
 
-"""
-Copyright 2014-2016, University of Bremen & DFKI GmbH Robotics Innovation Center
+# -------------------------------------------------------------------------------
+# This file is part of Phobos, a Blender Add-On to edit robot models.
+# Copyright (C) 2018 University of Bremen & DFKI GmbH Robotics Innovation Center
 
-This file is part of Phobos, a Blender Add-On to edit robot models.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-Phobos is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License
-as published by the Free Software Foundation, either version 3
-of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 
-Phobos is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with Phobos.  If not, see <http://www.gnu.org/licenses/>.
-
-File smurfs.py
-
-Created on 3 Nov 2016
-
-@author: Kai von Szadkowski
-"""
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------------------
 
 import yaml
 from datetime import datetime
