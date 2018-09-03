27eeb0eab301a9d1fbbe1246f004637ae825a411
Simon Reichel <simms1@web.de>
2018-09-03 16:56:37 +0200

    Add file headers to model files

--- phobos/model/joints.py
+++ phobos/model/joints.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python
 # coding=utf-8
 
+# -------------------------------------------------------------------------------
+# This file is part of Phobos, a Blender Add-On to edit robot models.
+# Copyright (C) 2018 University of Bremen & DFKI GmbH Robotics Innovation Center
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------------------
+
 """
 .. module:: phobos.exporter
     :platform: Unix, Windows, Mac
     :synopsis: TODO: INSERT TEXT HERE
 
 .. moduleauthor:: Kai von Szadowski, Simon Reichel
-
-Copyright 2014, University of Bremen & DFKI GmbH Robotics Innovation Center
-
-This file is part of Phobos, a Blender Add-On to edit robot models.
-
-Phobos is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License
-as published by the Free Software Foundation, either version 3
-of the License, or (at your option) any later version.
-
-Phobos is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with Phobos.  If not, see <http://www.gnu.org/licenses/>.
-
-File joints.py
-
-Created on 7 Jan 2014
 """
 
 import bpy
