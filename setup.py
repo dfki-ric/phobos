94ca140a35c444b5046bec0f4e047c3d87ef0418
Simon Reichel <simms1@web.de>
2018-09-03 17:06:30 +0200

    Add file headers to Makefile, setup etc

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3.5
+
+# -------------------------------------------------------------------------------
+# This file is part of Phobos, a Blender Add-On to edit robot models.
+# Copyright (C) 2018 University of Bremen & DFKI GmbH Robotics Innovation Center
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------------------
+
 """
 This module should be run to install Phobos properly.
 
