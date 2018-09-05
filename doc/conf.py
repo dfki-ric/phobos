4133e737b8bc4d42c2f894f3fae6f4f97e2f6369
Simon Reichel <simms1@web.de>
2018-09-05 16:40:51 +0200

    Add napoleon google docstring converter to sphinx conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,14 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+
+napoleaon_google_docstring = True
+napoleano_numpy_dosctring = False
+napoleon_use_ivar = True
+napoleon_use_param = True
+napoleon_use_keyword = True
+napoleon_use_rtype = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
