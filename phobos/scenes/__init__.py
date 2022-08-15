00b5bf6e7d9b2fdf0b238fb97538d1c4a8a73e63
Altin <altin.shala@web.de>
2022-08-15 13:05:05 +0200

    Restructuring of imports

--- phobos/scenes/__init__.py
+++ phobos/scenes/__init__.py
@@ -1,3 +1,12 @@
+from ..io import xml_factory
+
 from .scene import Scene
 from .assembly import Assembly
 from .entity import BaseEntity, Entity
+
+xml_factory.class_factory(Scene)
+xml_factory.class_factory(Assembly)
+xml_factory.class_factory(BaseEntity)
+xml_factory.class_factory(Entity)
+
+del xml_factory
