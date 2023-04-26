9abdca18664b12baa1544fc2e1a03592232e451f
Alex Zastrow <alexzastrow@posteo.de>
2023-04-26 22:01:05 +0200

    Removes submechanism TODO marks

--- phobos/blender/operators/editing.py
+++ phobos/blender/operators/editing.py
@@ -2856,7 +2856,6 @@ class DefineSubmodel(Operator):
         return {'FINISHED'}
 
 
-# [TODO v2.0.0] REVIEW this
 class AssignSubmechanism(Operator):
     """Assign a submechanism to the model"""
 
@@ -3072,7 +3071,6 @@ class AssignSubmechanism(Operator):
         return {'FINISHED'}
 
 
-# [TODO v2.0.0] REVIEW this
 class SelectSubmechanism(Operator):
     """Select all objects of a submechanism"""
 
@@ -3154,7 +3152,6 @@ class SelectSubmechanism(Operator):
         return {'FINISHED'}
 
 
-# [TODO v2.0.0] REVIEW this
 class DeleteSubmechanism(Operator):
     """Delete an existing submechanism"""
 
