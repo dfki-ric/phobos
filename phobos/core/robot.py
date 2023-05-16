Fix scaling issue

phobos/core/robot.py
@@ -2490,9 +2490,10 @@ class Robot(SMURFRobot):
         link = self.get_link(linkname)
         assert link is not None
         scale = np.array([scale_x, scale_y, scale_z])
+        assert all([s >0 for s in scale])
         for geo in link.visuals + link.collisions:
             _geo_scale = inv(geo.origin.to_matrix())[:3, :3].dot(scale)
-            geo.geometry.multiply_scale(_geo_scale)
+            geo.geometry.multiply_scale(np.abs(_geo_scale))
             # geo.geometry.apply_scale()
             geo.origin.xyz = [v*s for v, s in zip(geo.origin.xyz, scale)]
         joints = self.get_children(link.name)