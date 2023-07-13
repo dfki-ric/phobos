19760322e0522dc1ef56ae198d1b5d51ce05cd7e
hwiedPro <hwiedPro@users.noreply.github.com>
2023-07-13 13:53:02 +0000

    deploy: 75feedbb9abab5576623793ef832fd80a6655c56

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.0 (277da47)',
+    VERSION: '2.0.0 (75feedb)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
